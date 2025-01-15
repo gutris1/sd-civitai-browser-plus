e5470c576c2f7783bc1fcb087aae571fd3037585
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-01-15 20:33:48 +0700

    Update EncryptionImg.py

--- scripts/EncryptionImg.py
+++ scripts/EncryptionImg.py
@@ -300,7 +300,7 @@ if PILImage.Image.__name__ != 'EncryptedImage':
         msg = f'{AR} {TITLE} {RED}Disabled{RST}, Missing --encrypt-pass command line argument.'
     else:
         script_callbacks.on_app_started(app_started_callback)
-        msg = f'{AR} {TITLE} {BLUE}Enabled{RST}, Encryption Level {ORG}6{RST}' \
+        msg = f'{AR} {TITLE} {BLUE}Enabled{RST}, Encryption Level {ORG}7{RST}' \
               f'\n{AR} {TITLE} Check the release page for decrypting images in local Windows ' \
               f'https://github.com/gutris1/sd-encrypt-image'
 
